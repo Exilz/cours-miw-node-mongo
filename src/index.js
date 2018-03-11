96626699b1fb553d799fdaf00b88f96f537c8b48
Maxime Bertonnier <m.bertonnier@gmail.com>
2018-03-11 18:49:11 +0100

    Initialisation de la base de données au démarrage

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import express from 'express';
 import routes from './routes/index';
 import middlewares from './routes/middlewares';
+import initDB from './db/index';
 
 export default () => {
     const app = express();
 
-    // Initialisation des middlewares AVANT les routes
-    middlewares(app);
-    // Initialisation des routes
-    routes(app);
-    // Démarrage du serveur
-    app.listen(8080, () => {
-        console.log('Serveur démarré !');
-    });
+    initDB()
+    .then(() => {
+        // Initialisation des middlewares AVANT les routes
+        middlewares(app);
+        // Initialisation des routes
+        routes(app);
+        // Démarrage du serveur
+        app.listen(8080, () => {
+            console.log('Serveur démarré !');
+        });
+    })
+    .catch((err) => {
+        console.error(err);
+        process.exit(1);
+    })
 };
