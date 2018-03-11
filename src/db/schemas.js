0d2205b4fe6355a873867f01faa87f76ffd8a801
Maxime Bertonnier <m.bertonnier@gmail.com>
2018-03-11 18:45:48 +0100

    Création d'un premier schema mongoose

---
+++ src/db/schemas.js
@@ -0,0 +1,36 @@
+import { db } from '../index';
+import schemas from './'
+
+// Chaque modèle doit être initialisé à partir d'un schéma.
+// Pour ne pas recréer le modèle à chaque fois, on utilise une
+// petite abstraction qui stocke les modèles déjà créés et les
+// retourne si on y a déjà eu accès auparavant.
+let initializedModels = {};
+
+export function getModel (modelName) {
+    if (initializedModels[modelName]) {
+        return initializedModels[modelName];
+    }
+
+    const schema = schemas[modelName];
+    if (!schema) {
+        throw new Error(`Aucun schéma n'existe pour : ${modelName}`);
+    }
+
+    console.log(`Création du model pour ${modelName}...`);
+    const model = db.model(schema.collection, schema.definition, schema.collection);
+    initializedModels[modelName] = model;
+    return model;
+}
+
+const schemas = {
+    movies: {
+        collection: 'movies',
+        definition: {
+            title: String,
+            subtitle: String,
+            year: Number,
+            genres: [String]
+        }
+    }
+};
