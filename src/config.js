fea320f556c5c3522ddc6bc9c6bad0775c8d5cca
Maxime Bertonnier <m.bertonnier@gmail.com>
2018-03-15 18:14:41 +0100

    Ajout de la clé d'API de récupération des films

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
     PORT: 8080,
-    MONGO_URL: 'mongodb://faadev.les2cm.eu:1337/miw'
+    MONGO_URL: 'mongodb://faadev.les2cm.eu:1337/miw',
+    SALT: 'HA818tj6lYqq31iroEUd8t3R6b7Zi',
+    API_KEY: '64204564'
 };
